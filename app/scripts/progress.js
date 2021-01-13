05f466095de3a9dc376841e663b1e86cc54d39fd
Łukasz Fijał <cheapsik@gmail.com>
2021-01-13 16:33:49 +0100

    add footer

--- app/scripts/progress.js
+++ app/scripts/progress.js
@@ -2,25 +2,24 @@ const aboutDiv = document.querySelector(".about-wrapper");
 const bars = document.querySelectorAll(".progress");
 
 const scrollFunction = function () {
-    if (window.scrollY >= 200) {
+  if (window.scrollY >= 200) {
+    const interval = setInterval(() => {
+      bars.forEach(function (bar) {
+        let getWidth = parseFloat(bar.dataset.progress);
 
-     const interval = setInterval(() => {
-        bars.forEach(function(bar){
-          let getWidth = parseFloat(bar.dataset.progress);
-
-          for(let i = 0; i < getWidth; i++) {
-            bar.style.width = i + "%";
-          }
-          clearInterval(interval);
-        });
-      }, 500);
-      aboutDiv.setAttribute("style", "transform: translateY(0px); opacity: 1;");
-    } else {
-      bars.forEach((bar) =>{
-        bar.style.width = "0%";
+        for (let i = 0; i < getWidth; i++) {
+          bar.style.width = i + "%";
+        }
+        clearInterval(interval);
       });
-      aboutDiv.setAttribute("style", "transform: translateY(500px); opacity: 0;");
-    }
+    }, 500);
+    aboutDiv.setAttribute("style", "transform: translateY(0px); opacity: 1;");
+  } else {
+    bars.forEach((bar) => {
+      bar.style.width = "0%";
+    });
+    aboutDiv.setAttribute("style", "transform: translateY(500px); opacity: 0;");
+  }
 };
 
 window.addEventListener("scroll", scrollFunction);
